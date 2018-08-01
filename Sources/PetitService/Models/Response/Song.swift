9905ff38761d4f883c907ed640e8d0b7e0ecf6c1
Milan Vit <milanvit@milanvit.net>
2018-08-01 15:59:50 +0900

    Apparently duration is sometimes optional 🤦‍♂️

--- Sources/PetitService/Models/Response/Song.swift
+++ Sources/PetitService/Models/Response/Song.swift
@@ -4,7 +4,7 @@ public struct Song: Content {
     public let lyricsId: Int
     public let title: String
     public let artist: String
-    public let duration: Int
+    public let duration: Int?
     public let lyricsData: String?
     public let uploadDate: Date
     public let releaseDate: Date?

ec9fba1c3596a46d6a7e9539487857ee7a126d4c
Milan Vit <milanvit@milanvit.net>
2018-08-09 17:15:16 +0900

    Attempt to downgrade XMLCoding

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.6"),
-        .package(url: "https://github.com/LiveUI/XMLCoding.git", from: "0.1.1")
+        .package(url: "https://github.com/LiveUI/XMLCoding.git", .exact("0.1.1"))
     ],
     targets: [
         .target(name: "PetitService", dependencies: [
